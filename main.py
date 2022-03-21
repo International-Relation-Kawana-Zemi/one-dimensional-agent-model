544e3bb36d62b03770b782538aedb3eedb0fd8e2
kazuki <kaz.tokyo.tech20@gmail.com>
2022-03-21 21:59:43 +0900

    add update function

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import numpy as np
 
+from utils.function import update_function
+
+
 def update(array: np.ndarray) -> np.ndarray:
-    pass
+    _size: int = len(array)
+    random_sample_id_list = np.random.choice(_size, _size, replace=False)  # equal to permutation
+
+    res_array = np.zeros(shape=_size, dtype=np.float128)
+
+    for i in range(0, _size):
+        res_array[i] = update_function(array[random_sample_id_list[i]], array[random_sample_id_list[(i + 1) % _size]])
+    return res_array
+
 
 def main():
     _mu, _sigma = np.float128(0), np.float128(10)
